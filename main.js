b8e6acd9b40ce11ae4cc0427a48aae69c520a725
Your Name <you@example.com>
2017-05-10 21:20:13 -0700

    first commit

--- main.js
+++ main.js
@@ -1,8 +1,68 @@
-function chart() {
+window.onload = function() {
+    
 
-    function my() {
-        // generate chart here
-    }
+    var svg = d3.select("body").append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .append("g")
+        .attr("transform", "translate(" + width / 2 + "," + height * .52 + ")");
 
-    my.
-}
+    var partition = d3.layout.partition()
+        .sort(null)
+        .size([2 * Math.PI, radius * radius])
+        .value(function(d) { return 1; });
+
+    var arc = d3.svg.arc()
+        .startAngle(function(d) { return d.x; })
+        .endAngle(function(d) { return d.x + d.dx; })
+        .innerRadius(function(d) { return Math.sqrt(d.y); })
+        .outerRadius(function(d) { return Math.sqrt(d.y + d.dy); });
+
+    d3.json("data.json", function(error, root) {
+        if (error) throw error;
+
+        var path = svg.datum(root).selectAll("path")
+            .data(partition.nodes)
+            .enter().append("path")
+            .attr("display", function(d) { return d.depth ? null : "none"; }) // hide inner ring
+            .attr("d", arc)
+            .style("stroke", "#fff")
+            .style("fill", function(d) { return color((d.children ? d : d.parent).name); })
+            .style("fill-rule", "evenodd")
+            .each(stash);
+
+        d3.selectAll("input").on("change", function change() {
+            var value = this.value === "count"
+                ? function() { return 1; }
+                : function(d) { return d.size; };
+
+            path
+                .data(partition.value(value).nodes)
+            .transition()
+                .duration(1500)
+                .attrTween("d", arcTween);
+        });
+
+        // Stash the old values for transition.
+        function stash(d) {
+            d.x0 = d.x;
+            d.dx0 = d.dx;
+        }
+
+        // Interpolate the arcs in data space.
+        function arcTween(a) {
+            var i = d3.interpolate({x: a.x0, dx: a.dx0}, a);
+            return function(t) {
+                var b = i(t);
+                a.x0 = b.x;
+                a.dx0 = b.dx;
+                return arc(b);
+            };
+        }
+
+        d3.select(self.frameElement).style("height", height + "px");
+
+    });
+};
+    
+    
---
+++ sunburst.js
@@ -0,0 +1,10 @@
+function sunburst() {
+    var width = 960,
+        height = 700,
+        radius = Math.min(width, height) / 2,
+        color = d3.scale.category20c();
+
+    function my() {
+        // generate chart here
+    }
+}
